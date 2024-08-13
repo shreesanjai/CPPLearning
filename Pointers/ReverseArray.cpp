8221f997616f44e3d8b1126b7d43a4e6f0df877f
Shree <shreesanjai3@gmail.com>
2024-08-13 17:33:11 +0530

    Day 2 final

--- Pointers/ReverseArray.cpp
+++ Pointers/ReverseArray.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main()
 {
 
-    int n = 5;
+    int n;
+    cin>>n;
 
     int *ptr = new int;
 
@@ -11,12 +12,12 @@ int main()
         cin >> *(ptr + i);
 
     for (int i = 0; i < n; i++)
-        cout << *(ptr + i) << "-";
+        cout << *(ptr + i) << " - ";
     cout << endl;
 
-    for (int i = 0; i < n/2; i++)
+    for (int i = 0; i < n-1; i++)
     {
-        int temp = *(ptr+i);
+        int temp = *(ptr+i) ;
         *(ptr+i) = *(ptr+n-1-i);
         *(ptr+n-1-i) = temp;
     }
